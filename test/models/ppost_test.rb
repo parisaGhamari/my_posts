771826754547fe65832d049cedba10d4c6ace700
user <parisaghamari56@gmail.com>
2024-01-28 12:27:23 -0800

    this is commit.

--- test/models/ppost_test.rb
+++ test/models/ppost_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class PpostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" 'this title' 'this body' do
+    assert true
+  end
+  test "the truth2" 'this title2' 'this body2' do
+    assert true
+  end
+  test "the truth3" 'this title3' 'this body3' do
+    assert true
+  end
 end
---
+++ test/spec/ppost_spec.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class PpostTest < ActiveSupport::TestCase
+   test "the truth" do
+     assert true
+     (expect(true))
+   end
+end
