ce2fecc04eef42c32d17a1aa051cffa1ae103324
user <parisaghamari56@gmail.com>
2024-01-29 11:18:40 -0800

    this is commit.

--- spec/models/ppost_spec.rb
+++ spec/models/ppost_spec.rb
@@ -1,47 +1,36 @@
-# require "test_helper"
-#
-# class PpostTest < ActiveSupport::TestCase
-#   test "the truth" 'this title' 'this body' do
-#     assert true
-#   end
-#   test "the truth2" 'this title2' 'this body2' do
-#     assert true
-#   end
-#   test "the truth3" 'this title3' 'this body3' do
-#     assert true
-#   end
-# end
-# require = require "test_helper"
-#
-# class PostTest < ActiveSupport::TestCase
-#   # test "the truth" do
-#   #   assert true
-#   # end
-# end
-
 require 'rails_helper'
 
 RSpec.describe Ppost, type: :model do
-  it "is valid with a name and title and body" do
-    post = Ppost.new(name:"my name is "  ,title: "My first post", body: "This is the body of my first post.")
+  it 'can run tests' do
+    expect(true).to be(false)
+  end
+  describe 'attributes' do
+    it { should respond_to(:name) }
+    it { should respond_to(:title) }
+    it { should respond_to(:body) }
+
+  end
+
+end
+
+
+
+
+RSpec.describe Ppost, type: :model do
+  it 'is valid with a title and body' do
+    post = Ppost.new(title: 'My first post', body: 'This is the body of my first post.')
     expect(post).to be_valid
   end
 
-  it "is invalid without a title" do
-    post = Ppost.new(name:"my name ",title: nil, body: "This is a body without a title.")
+  it 'is invalid without a title' do
+    post = Ppost.new(title: nil, body: 'This is a body without a title.')
     post.valid?
     expect(post.errors[:title]).to include("can't be blank")
   end
 
-  it "is invalid without a body" do
-    post = Ppost.new(name:"my name ",title: "my post ", body: nil)
+  it 'is invalid without a body' do
+    post = Ppost.new(title: 'Title only', body: nil)
     post.valid?
     expect(post.errors[:body]).to include("can't be blank")
-  end
-
-  it "is invalid without a name" do
-    post = Ppost.new(name: nil,title: "Title only", body: "This is a body without a title.")
-    post.valid?
-    expect(post.errors[:name]).to include("can't be blank")
   end
 end
